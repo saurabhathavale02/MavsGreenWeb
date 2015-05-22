d62c980f139ef0788f410d342f0808830a3c1372
shrikant <shriku4u27@gmail.com>
2015-05-21 22:08:23 -0500

    Boilerplate
    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MavsGreenWeb::Application.config.secret_key_base = '72afb7518732a44cfd1f7789e735341be8471a607c5d457cffae355afb5e3d966c70364224b7f6b58a6cf149b0192938b917500074a576a36d9f9eb8cd264f6e'
